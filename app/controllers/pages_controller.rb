42f4fb04dfb18f43dbdf44b88cbfe1806882b2fc
Jack Holdsworth <jackch90@gmail.com>
2020-03-08 19:43:25 +0000

    new Profile cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    # binding.pry
+    @user = current_user
     if params[:query].present?
-      # redirect_to :home if Category.find_by(name: params[:query]).projects.nil?
-      # @project_list = Category.find_by(name: params[:query]).projects
       @project_list = Project.where(published: true, category: params[:query])
     else
       @project_list = Project.where(published: true)
